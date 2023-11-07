5673f6e36ab4e81fd14c949d2c7cd39cb6226954
Sergioleguizamon <142453246+Sergioleguizamon@users.noreply.github.com>
2023-11-06 23:52:58 -0300

    hay un monton de cambios que no puedo explicar

--- estiloPaginaWeb2.js
+++ estiloPaginaWeb2.js
@@ -1,25 +1,48 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const carousel = document.querySelector(".carousel");
-    const images = carousel.querySelector(".images");
-    const prevButton = carousel.querySelector(".prev");
-    const nextButton = carousel.querySelector(".next");
+     let indice = 0;
+const imagenes = ['./img/cid.jpg', './img/MasterShadow.jpg', './img/john smith.png'];
+const fondo = document.querySelector("#carrusel");
+const selectCarrusel = document.getElementById("selectCarrusel");
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
 
-    let currentIndex = 0;
-    const totalImages = images.children.length;
-    const imageWidth = images.children[0].offsetWidth;
+// Llenar el select con opciones basadas en las imágenes
+for (let i = 0; i < imagenes.length; i++) {
+    const option = document.createElement("option");
+    option.value = i;
+    option.text = `Imagen ${i + 1}`;
+    selectCarrusel.appendChild(option);
+}
 
-    prevButton.addEventListener("click", function () {
-        currentIndex = (currentIndex - 1 + totalImages) % totalImages;
-        updateCarousel();
-    });
-
-    nextButton.addEventListener("click", function () {
-        currentIndex = (currentIndex + 1) % totalImages;
-        updateCarousel();
-    });
-
-    function updateCarousel() {
-        const translateX = -currentIndex * imageWidth;
-        images.style.transform = `translateX(${translateX}px)`;
-    }
+// Función para cambiar la imagen en función de la selección del select
+selectCarrusel.addEventListener("change", () => {
+    indice = parseInt(selectCarrusel.value);
+    mostrarImagenSeleccionada();
 });
+
+// Función para mostrar la imagen actual
+function mostrarImagenSeleccionada() {
+    let imagenActual = imagenes[indice];
+    fondo.style.backgroundImage = `url('${imagenActual}')`;
+    fondo.style.backgroundSize = "cover";
+}
+
+prev.addEventListener("click", () => {
+    if (indice === 0) {
+        indice = imagenes.length - 1;
+    } else {
+        indice--;
+    }
+    mostrarImagenSeleccionada();
+});
+
+next.addEventListener("click", () => {
+    if (indice === imagenes.length - 1) {
+        indice = 0;
+    } else {
+        indice++;
+    }
+    mostrarImagenSeleccionada();
+});
+
+// Mostrar la imagen inicial
+mostrarImagenSeleccionada();
